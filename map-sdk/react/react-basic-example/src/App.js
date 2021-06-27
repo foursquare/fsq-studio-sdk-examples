bc02e09a92dee3cb118ff611cb6d414550a17c70
Ilya Boyandin <iboyandin@foursquare.com>
2021-06-27 10:23:24 +0200

    Fix: Missing React import
    The app fails to load on [StackBlitz](https://stackblitz.com/github/UnfoldedInc/examples/tree/master/map-sdk/react/react-basic-example) because of that

--- map-sdk/react/react-basic-example/src/App.js
+++ map-sdk/react/react-basic-example/src/App.js
@@ -1,4 +1,4 @@
-import {useState, useRef, useEffect} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {createMap, setViewState, getLayers, setLayerVisibility} from '@unfolded/map-sdk';
 
 import './App.scss';
