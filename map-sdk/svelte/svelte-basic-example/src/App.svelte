ffbd2292f37763eb9f5729064c65554364809fe5
Marko Letic <marc.letic@gmail.com>
2021-10-27 20:51:34 +0200

    Add Svelte Map SDK example

---
+++ map-sdk/svelte/svelte-basic-example/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import OverlayMenu from './OverlayMenu.svelte';
+  import {UnfoldedMap, setViewState} from '@unfolded/map-sdk';
+
+  let isMapLoaded = false;
+
+  const unfoldedMap = new UnfoldedMap({
+    mapUUID: '80c800cc-5805-4416-94a5-bd8105cab7f7',
+    embed: true,
+    onLoad: () => {
+      console.log('%cUnfoldedMapSDK: %cMap has loaded...', 'color: violet;', 'color: yellow;');
+      isMapLoaded = true;
+    }
+  });
+</script>
+
+<OverlayMenu {isMapLoaded} {unfoldedMap} />
