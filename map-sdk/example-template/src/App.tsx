1030ece12f25626001f51b493b9d9ab44833299d
Dragan Okanovic <dragan.okan@gmail.com>
2023-08-24 20:52:49 +0200

    bla

--- map-sdk/example-template/src/App.tsx
+++ map-sdk/example-template/src/App.tsx
@@ -1,15 +1,16 @@
-import React, {FC, useEffect, useRef, useState} from 'react';
+import {FC, useEffect, useRef, useState} from 'react';
 import {createMap, MapApi} from '@unfolded/map-sdk';
 
 export const App: FC = () => {
-  const containerRef = useRef(null);
-  const [map, setMap] = useState<MapApi | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [, setMap] = useState<MapApi | null>(null);
 
   useEffect(() => {
     const initMap = async () => {
-      const map = await createMap();
+      const map = await createMap({
+        container: containerRef.current!
+      });
 
-      map.addToDOM(containerRef.current!);
       setMap(map);
 
       // here you add map.thing()
@@ -18,7 +19,5 @@ export const App: FC = () => {
     initMap();
   }, []);
 
-  return (
-    <div style={{width: '100vw', height: '100vh', overflow: 'hidden'}} ref={containerRef}></div>
-  );
+  return <div id="map-container" ref={containerRef}></div>;
 };
