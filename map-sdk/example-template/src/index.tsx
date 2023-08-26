3e4a5a6a374d68c1da4b9cddb7bbe2195f622cca
Dragan Okanovic <dragan.okan@gmail.com>
2023-08-26 18:47:23 +0200

    Sample with buttons

--- map-sdk/example-template/src/index.tsx
+++ map-sdk/example-template/src/index.tsx
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client';
 import './index.css';
-import {App} from './App';
+import {App, AppWithCustomUI} from './App';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(<AppWithCustomUI />);
