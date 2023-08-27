fe2d33497df26475bc2f81a31291cac63e1db8a2
Dragan Okanovic <dragan.okan@gmail.com>
2023-08-28 00:15:48 +0200

    remove example template

--- map-sdk/example-template/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import {App, AppWithCustomUI} from './App';
-
-createRoot(document.getElementById('root')!).render(<AppWithCustomUI />);
