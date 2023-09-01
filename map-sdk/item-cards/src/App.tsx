ee68b82bd5f1db1b560ba2798172a70e6934fe89
Dragan Okanovic <dragan.okan@gmail.com>
2023-09-01 21:21:11 +0200

    Add hover cards

--- map-sdk/item-cards/src/App.tsx
+++ map-sdk/item-cards/src/App.tsx
@@ -1,9 +1,24 @@
-import {FC, useCallback, useEffect, useRef, useState} from 'react';
+import {FC, useEffect, useRef, useState} from 'react';
 import {createMap, MapApi} from '@unfolded/map-sdk';
+
+// TODO generic Dataset<T>
+const citiesDataset = {
+  label: 'Sample Dataset',
+  data: [
+    {name: 'Beglrade', latitude: 44.8125, longitude: 20.4612},
+    {name: 'San Francisco', latitude: 37.7749, longitude: -122.4194},
+    {name: 'New York', latitude: 40.7128, longitude: -74.006}
+  ]
+};
 
 export const App: FC = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<MapApi | null>(null);
+  const [selectionInfo, setSelectionInfo] = useState<null | {
+    idx: number;
+    latOffset: number;
+    lngOffset: number;
+  }>(null);
 
   useEffect(() => {
     const initMap = async () => {
@@ -13,81 +28,57 @@ export const App: FC = () => {
 
       setMap(map);
 
-      // here you add map.thing()
-    };
-
-    initMap();
-  }, []);
-
-  return <div id="map-container" ref={containerRef}></div>;
-};
-
-export const AppWithCustomUI: FC = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [map, setMap] = useState<MapApi | null>(null);
-
-  useEffect(() => {
-    const initMap = async () => {
-      const map = await createMap({
-        container: containerRef.current!
+      map.addDataset(citiesDataset, {
+        autoCreateLayers: true,
+        centerMap: true
       });
 
-      setMap(map);
+      const cfg = map.getMapConfig();
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (cfg as any).config.visState.interactionConfig.tooltip.enabled = false;
+      map.setMapConfig(cfg);
+      console.log(cfg);
     };
 
     initMap();
   }, []);
 
-  const logLayers = useCallback(() => {
+  useEffect(() => {
     if (!map) {
       return;
     }
 
-    const layers = map.getLayers();
-    console.log(layers);
-  }, [map]);
-
-  const addSampleLayer = useCallback(() => {
-    if (!map) {
-      return;
-    }
-
-    const sampleDataset = {
-      id: 'my-sample-dataset',
-      label: 'Sample Dataset',
-      data: [
-        {latitude: 44.8125, longitude: 20.4612}, // Belgrade
-        {latitude: 37.7749, longitude: -122.4194}, // San Francisco
-        {latitude: 40.7128, longitude: -74.006} // New York
-      ]
+    map.eventHandlers.onHover = e => {
+      if (e.pickInfo && e.pickInfo.rowIndex !== selectionInfo?.idx) {
+        setSelectionInfo({
+          idx: e.pickInfo.rowIndex,
+          latOffset: e.position[0],
+          lngOffset: e.position[1]
+        });
+      } else {
+        setSelectionInfo(null);
+      }
     };
 
-    map.addDataset(sampleDataset, {
-      autoCreateLayers: true,
-      centerMap: true
-    });
+    map.eventHandlers.onViewUpdate = e => {
+      // TODO
+    };
   }, [map]);
 
   return (
     <>
       <div id="map-container" ref={containerRef}></div>
-      <div className="controls">
-        <button
-          onClick={() => {
-            logLayers();
+      {selectionInfo && (
+        <div
+          id="item-card"
+          style={{
+            left: selectionInfo.latOffset,
+            top: selectionInfo.lngOffset
           }}
         >
-          log layers
-        </button>
-
-        <button
-          onClick={() => {
-            addSampleLayer();
-          }}
-        >
-          add sample layer
-        </button>
-      </div>
+          {citiesDataset.data[selectionInfo.idx].name}
+        </div>
+      )}
     </>
   );
 };
