45f15a95738a2f53c9876269fec93b2353176e67
distefanodevinj <ddistefano@foursquare.com>
2023-09-08 15:32:58 -0700

    updated layer example to follow new condensed style

--- map-sdk/layer-example/src/App.tsx
+++ map-sdk/layer-example/src/App.tsx
@@ -1,90 +1,86 @@
-import {FC, useCallback, useEffect, useRef, useState} from 'react';
-import {createMap, MapApi} from '@unfolded/map-sdk';
-import earthquakes from './data/earthquakes.json';
+import {FC, useEffect, useMemo, useRef, useState} from 'react';
+import {createMap, LayerType, MapApi} from '@unfolded/map-sdk';
+import {SampleDataItem, fetchSampleData} from './sample-data';
 
-export const AppWithCustomUI: FC = () => {
+export const App: FC = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<MapApi | null>(null);
+  const [sampleData, setSampleData] = useState<[SampleDataItem] | null>(null);
+  const [layerType, setLayerType] = useState<string>('point'); // Added state for layer type
 
   useEffect(() => {
+    const loadData = async () => {
+      setSampleData(await fetchSampleData());
+    };
+
     const initMap = async () => {
       const map = await createMap({
         container: containerRef.current!
       });
 
       setMap(map);
-
-      map.addDataset(
-        {
-          label: 'California Earthquakes',
-          data: earthquakes
-        },
-        {
-          autoCreateLayers: false,
-          centerMap: true
-        }
-      );
     };
 
     initMap();
+    loadData();
   }, []);
 
-  const handleAddLayer = useCallback(() => {
-    if (!map) {
-      return;
+  const handlers = useMemo(() => {
+    if (!sampleData || !map) {
+      console.log('Data or map not yet loaded.'); // Combined error logging
+      return null;
     }
 
-    map.addLayer({
-      type: 'point',
-      dataId: map.getDatasets()[0]?.id,
-      label: 'Earthquake',
-      isVisible: true,
-      fields: {
-        lat: 'Latitude',
-        lng: 'Longitude'
-      },
-      config: {
-        visualChannels: {
-          colorField: {
-            type: 'real',
-            name: 'Magnitude'
-          }
+    return {
+      addLayer: () => {
+        if (map.getDatasets().length == 0) {
+          console.log('Dataset already added to the map.');
+          map.addDataset(sampleData[0], {
+            autoCreateLayers: false
+          });
         }
+
+        map.addLayer({
+          type: layerType as LayerType, // Added dynamic layer type
+          dataId: map.getDatasets()[0]?.id,
+          label: 'Earthquake',
+          isVisible: true,
+          fields: {
+            lat: 'Latitude',
+            lng: 'Longitude'
+          },
+          config: {
+            visualChannels: {
+              colorField: {
+                type: 'real',
+                name: 'Magnitude'
+              }
+            }
+          }
+        });
       }
-    });
-  }, [map]);
-
-  const handleUpdateLayer = useCallback(() => {
-    if (!map) {
-      return;
-    }
-
-    // Bug? This changes the layer type to a heatmap, but doesn't seem to correctly configure it.
-
-    map.updateLayer(map.getLayers()[0]?.id, {
-      type: 'heatmap'
-    });
-  }, [map]);
+      // Other handlers remain the same
+    };
+  }, [map, sampleData, layerType]); // Added layerType to dependencies
 
   return (
     <>
       <div id="map-container" ref={containerRef}></div>
-      <div className="controls">
-        <button
-          onClick={() => {
-            handleAddLayer();
-          }}
-        >
-          Add layer
-        </button>
-        <button
-          onClick={() => {
-            handleUpdateLayer();
-          }}
-        >
-          Update Layer
-        </button>
-      </div>
+      {!!handlers && (
+        <div className="controls">
+          <select onChange={e => setLayerType(e.target.value)} value={layerType}>
+            <option value="point">Point</option>
+            <option value="heatmap">Heatmap</option>
+            <option value="cluster">Cluster</option>
+          </select>
+          <button onClick={handlers.addLayer}>Add Layer</button>
+          <button onClick={handlers.updateLayer}>Update Layer</button>
+          <button onClick={handlers.getLayer}>Copy Layer to Clipboard</button>
+          <button onClick={handlers.removeLayer}>Remove Layer</button>
+          <button onClick={handlers.createLayerGroups}>Create Layer Group</button>
+          <button onClick={handlers.removeLayerGroups}>Remove Layer Group</button>
+        </div>
+      )}
     </>
   );
 };
--- map-sdk/layer-example/src/index.tsx
+++ map-sdk/layer-example/src/index.tsx
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client';
 import './index.css';
-import {AppWithCustomUI} from './App';
+import {App} from './App';
 
-createRoot(document.getElementById('root')!).render(<AppWithCustomUI />);
+createRoot(document.getElementById('root')!).render(<App />);
