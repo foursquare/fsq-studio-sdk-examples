2e172352afd5387ae62e554a3ff9e04d508532b8
Dragan Okanovic <dragan.okan@gmail.com>
2023-09-09 08:49:08 +0200

    Cleanup

--- map-sdk/basic-example/src/App.tsx
+++ map-sdk/basic-example/src/App.tsx
@@ -1,4 +1,4 @@
-import {FC, useCallback, useEffect, useRef, useState} from 'react';
+import {FC, useEffect, useRef, useState} from 'react';
 import {createMap, MapApi} from '@unfolded/map-sdk';
 
 export const App: FC = () => {
@@ -12,82 +12,14 @@ export const App: FC = () => {
       });
 
       setMap(map);
-
-      // here you add map.thing()
     };
 
     initMap();
   }, []);
-
-  return <div id="map-container" ref={containerRef}></div>;
-};
-
-export const AppWithCustomUI: FC = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [map, setMap] = useState<MapApi | null>(null);
 
   useEffect(() => {
-    const initMap = async () => {
-      const map = await createMap({
-        container: containerRef.current!
-      });
-
-      setMap(map);
-    };
-
-    initMap();
-  }, []);
-
-  const logLayers = useCallback(() => {
-    if (!map) {
-      return;
-    }
-
-    const layers = map.getLayers();
-    console.log(layers);
+    map && console.log(map.getMapConfig());
   }, [map]);
 
-  const addSampleLayer = useCallback(() => {
-    if (!map) {
-      return;
-    }
-
-    const sampleDataset = {
-      id: 'my-sample-dataset',
-      label: 'Sample Dataset',
-      data: [
-        {latitude: 44.8125, longitude: 20.4612}, // Belgrade
-        {latitude: 37.7749, longitude: -122.4194}, // San Francisco
-        {latitude: 40.7128, longitude: -74.006} // New York
-      ]
-    };
-
-    map.addDataset(sampleDataset, {
-      autoCreateLayers: true,
-      centerMap: true
-    });
-  }, [map]);
-
-  return (
-    <>
-      <div id="map-container" ref={containerRef}></div>
-      <div className="controls">
-        <button
-          onClick={() => {
-            logLayers();
-          }}
-        >
-          log layers
-        </button>
-
-        <button
-          onClick={() => {
-            addSampleLayer();
-          }}
-        >
-          add sample layer
-        </button>
-      </div>
-    </>
-  );
+  return <div id="map-container" ref={containerRef}></div>;
 };
