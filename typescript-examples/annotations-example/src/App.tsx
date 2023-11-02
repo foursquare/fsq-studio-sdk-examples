b3e1b3acbd328e57eb8218dda8e6273140676a3e
Dragan Okanovic <1355455+abstractalgo@users.noreply.github.com>
2023-11-02 11:44:13 +0100

    Update examples (#71)

---
+++ typescript-examples/annotations-example/src/App.tsx
@@ -0,0 +1,37 @@
+import { FC, useEffect, useRef } from "react";
+import { createMap } from "@unfolded/map-sdk";
+import { editorContent } from "./editor-content";
+
+export const App: FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const initMap = async () => {
+      const map = await createMap({
+        container: containerRef.current!,
+      });
+
+      map.addAnnotation({
+        id: "my-annotation",
+        kind: "POINT",
+        isVisible: true,
+        autoSize: true,
+        autoSizeY: true,
+        anchorPoint: [-122.4194, 37.7749],
+        label: "Richtext annotation",
+        editorState: editorContent,
+        lineColor: "#C32899",
+        lineWidth: 5,
+        textWidth: 167.2265625,
+        textHeight: 75,
+        textVerticalAlign: "bottom",
+        armLength: 50,
+        angle: -45,
+      });
+    };
+
+    initMap();
+  }, []);
+
+  return <div id="map-container" ref={containerRef}></div>;
+};
